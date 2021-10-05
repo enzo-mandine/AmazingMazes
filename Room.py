efadf10a3dd947437770c46aee6896d95b95d3a3
enzo-mandine <55872776+enzo-mandine@users.noreply.github.com>
2021-10-05 11:39:29 +0200

    addclass: Room, Maze

---
+++ Room.py
@@ -0,0 +1,51 @@
+class Room:
+
+    # Each room have 4 opening possible
+    # By default each wall is closed and set to True
+    # If the room have been visited we should not consider it anymore and set visited to True
+
+    def __init__(self):
+        self.name = "room"
+        self.N = True
+        self.S = True
+        self.E = True
+        self.W = True
+        self.visited = False
+
+    def getName(self):
+        return self.name
+
+    def getN(self):
+        return self.N
+
+    def setN(self, boolean):
+        self.N = boolean
+        return self
+
+    def getS(self):
+        return self.S
+
+    def setS(self, boolean):
+        self.S = boolean
+        return self
+
+    def getE(self):
+        return self.E
+
+    def setE(self, boolean):
+        self.E = boolean
+        return self
+
+    def getW(self):
+        return self.W
+
+    def setW(self, boolean):
+        self.W = boolean
+        return self
+
+    def getIsVisited(self):
+        return self.visited
+
+    def setIsVisited(self, boolean):
+        self.visited = boolean
+        return self
