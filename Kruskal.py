d2a7a88d67b0828d65fff30c0fcf3c61b7cb6a2a
enzo-mandine <55872776+enzo-mandine@users.noreply.github.com>
2021-10-11 20:33:19 +0200

    update Kruskal

--- Kruskal.py
+++ Kruskal.py
@@ -12,6 +12,19 @@ class Matrix:
         self._setAllRooms()
         self._setAllWalls()
 
+        self._resolve()
+
+    def _setRoomWalls(self):
+        for wall in self.allWalls:
+            print(wall)
+            a = self._getRoom((wall[0], wall[1]))
+            b = self._getRoom((wall[2], wall[3]))
+            a.openWall(b.getPosition())
+            b.openWall(a.getPosition())
+
+    def _getRoom(self, pos):
+        return self.matrix[pos[0]][pos[1]]
+
     def _setAllWalls(self):
         walls = [(x, y, x + 1, y)
                  for x in range(self.size - 1)
@@ -32,17 +45,17 @@ class Matrix:
     def _setMatrix(self):
         matrix = []
         idx = 0
-        for y in range(0, self.size):
+        for row in range(0, self.size):
             matrix.append([])
-            for x in range(0, self.size):
-                room = Room(y, x, idx)
-                matrix[y].append(room)
+            for col in range(0, self.size):
+                room = Room(row, col, idx)
+                matrix[row].append(room)
                 idx += 1
         self.matrix = matrix
 
         return True
 
-    def resolve(self):
+    def _resolve(self):
         walls_copy = self.allWalls[:]
         random.shuffle(walls_copy)
 
@@ -55,38 +68,46 @@ class Matrix:
                     set_a = s
                 if (wall[2], wall[3]) in s:
                     set_b = s
+
             if set_a is not set_b:
                 self.allRooms.remove(set_a)
                 self.allRooms.remove(set_b)
                 self.allRooms.append(set_a.union(set_b))
                 self.allWalls.remove(wall)
+
+        self._setRoomWalls()
+        return self
     # end Matrix
 
 
 class Room:
     def __init__(self, y, x, idx):
-        self.x = y
-        self.y = x
+        self.y = y
+        self.x = x
         self.idx = idx
         self.neighbours = None
-        self.isWall = {'n': True, 's': True, 'e': True, 'w': True}
+        self.isWall = {'n': False, 's': False, 'e': False, 'w': False}
 
     def openWall(self, position):
-        if position == (self.x, self.y - 1):
-            self.isWall['n'] = False
-        if position == (self.x, self.y + 1):
-            self.isWall['s'] = False
-        if position == (self.x + 1, self.y):
-            self.isWall['e'] = False
-        if position == (self.x - 1, self.y):
-            self.isWall['w'] = False
+        if position == (self.y - 1, self.x):
+            self.isWall['n'] = True
+        if position == (self.y + 1, self.x):
+            self.isWall['s'] = True
+        if position == (self.y, self.x + 1):
+            self.isWall['e'] = True
+        if position == (self.y, self.x - 1):
+            self.isWall['w'] = True
+        return self
 
     def getPosition(self):
-        return self.x, self.y
+        return self.y, self.x
     # end Room
 
 
 size = int(input('dimension:'))
 matrix = Matrix(size)
-matrix.resolve()
-print'walls:', matrix.allWalls
+for row in matrix.matrix:
+    for col in row:
+        print col.getPosition(), col.isWall
+# print'walls:', matrix.allWalls
+# print'rooms:', matrix.allRooms
